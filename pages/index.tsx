b82c55c882c6664706ab1da2486ae17353002adb
lastnamearya <arya.jigyasu6815@gmail.com>
2023-02-14 18:19:14 +0530

    fix: ts error

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ export async function getServerSideProps() {
   };
 }
 
-export default function Home({ githubStars }) {
+export default function Home({ githubStars }: any) {
   return (
     <>
       <Head>
